d040dbcec93d5c4d0a5c94019b0cc43c9a23a82f
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-08-17 17:15:20 +0700

    Validasi dengan Class Form Request

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\SubscriptionFormRequest;
 
 class HomeController extends Controller
 {
@@ -11,12 +11,8 @@ class HomeController extends Controller
         return view('home');
     }
 
-    public function store(Request $request)
+    public function store(SubscriptionFormRequest $request)
     {
-        $this->validate($request, [
-            'email' => 'required|email'
-        ]);
-
-        return dd($request->email);
+        return 'Berhasil Langganan!';
     }
 }
