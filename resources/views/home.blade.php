a04c3db72a20ece8d6e0e2b3e228c45a24e5b150
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-08-17 17:00:29 +0700

    Validasi Form dengan Gaya Bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,18 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Mengenal Validasi Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 </head>
 <body>
-    <form action="{{ route('subscribe') }}" method="POST">
-        @csrf
+    <div class="container">
+        <form action="{{ route('subscribe') }}" method="POST">
+            @csrf
 
-        <label for="">
-            <input type="text" name="email" id="">
-        </label>
-        @if ($errors->has('email'))
-            {{ $errors->first('email') }}
-        @endif
-        <input type="submit" value="Subscribe">
-    </form>
+            <div class="form-group">
+                <input type="text" name="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" placeholder="Masukkan email">
+                
+                @if ($errors->has('email'))
+                    <div class="invalid-feedback">
+                        {{ $errors->first('email') }}
+                    </div>
+                @endif
+            </div>
+
+            <input type="submit" class="btn btn-primary" value="Subscribe">
+        </form>
+    </div>
 </body>
 </html>
