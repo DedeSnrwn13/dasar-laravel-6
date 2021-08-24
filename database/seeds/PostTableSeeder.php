94b282926a5a90117d7c57fac43b0c3fb466c4cb
DedeSnrwn13 <sunarwandede13@gmail.com>
2021-08-24 13:38:32 +0700

    Seeder Factory

--- database/seeds/PostTableSeeder.php
+++ database/seeds/PostTableSeeder.php
@@ -6,15 +6,6 @@ use Faker\Generator as Faker;
 
 class PostTableSeeder extends Seeder
 {
-    protected $post;
-    protected $faker;
-
-    public function __construct(Post $post, Faker $faker)
-    {
-        $this->post = $post;
-        $this->faker = $faker;
-    }
-
     /**
      * Run the database seeds.
      *
@@ -22,16 +13,6 @@ class PostTableSeeder extends Seeder
      */
     public function run()
     {
-        foreach(range(1, 100) as $x) {
-            $this->post->create([
-                'title' => $this->faker->sentence(5),
-                'content' => $this->faker->sentence(50)
-            ]);
-        }
-
-        // Post::create([
-        //     'title' => 'Totle 1',
-        //     'content' => 'Example Content 1'
-        // ]);
+        factory(Post::class, 120)->create();
     }
 }
